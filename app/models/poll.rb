20867ca87d78a260f8bf91a91773e9f35a0a9d57
gkenenitz <gkenenitz@apple.com>
2020-12-29 02:03:00 +0900

    Get total score relative to a poll & given option_id

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,3 +1,9 @@
 class Poll < ApplicationRecord
   has_many :options
+  has_many :user_options, through: :options
+
+  def option_score(option_id)
+    opts = user_options.where(option_id: option_id)
+    opts.pluck(:score).inject(:+)
+  end
 end
--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -1,10 +1,27 @@
 require "rails_helper"
 
 describe Poll do
-  let(:poll) { create(:poll) }
+  let(:poll) { create(:poll, name: "January 2021") }
 
-  describe "#poll_option_score" do
+  describe "#option_score" do
+    let(:iliad) { create(:option, name: "The Iliad", poll: poll) }
+    let(:odyssey) { create(:option, name: "The Odyssey", poll: poll) }
+
+    let(:betty) do
+      create(:user, first_name: "Betty", last_name: "Boop", email: "betty@boop.com")
+    end
+
+    let(:liz) do
+      create(:user, first_name: "Liz", last_name: "Loop", email: "liz@loop.com")
+    end
+
     it "calculates the total for a given poll option" do
+      create(:user_option, user: liz, option: odyssey, score: 8)
+      create(:user_option, user: betty, option: odyssey, score: 7)
+
+      create(:user_option, user: liz, option: iliad, score: 2)
+
+      expect(poll.option_score(odyssey.id)).to be(15)
     end
   end
 end
