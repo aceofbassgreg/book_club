4aade918227e534f3dbaf49523ce55a3ac388cd2
gkenenitz <gkenenitz@apple.com>
2021-01-03 21:17:39 +0900

    AR Validations for user:  min 10 length for password, unique email required

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ require 'bcrypt'
 class User < ApplicationRecord
   has_secure_password
   has_many :user_options
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 10 }
 end
