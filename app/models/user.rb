8fd0bc7563d38eda741d40afefd7637b7a40cae8
gkenenitz <gkenenitz@apple.com>
2021-01-09 14:23:03 +0900

    refactor query to private method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,14 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 10 }
 
   def finished_voting_in_poll?(poll_id)
-    votes = UserOption.joins(:option).where(option: { poll_id: poll_id })
+    votes = votes_for_poll(poll_id)
     options = Poll.find(poll_id).options
     votes.pluck(:option_id).sort == options.pluck(:id).sort && !options.empty?
+  end
+
+  private
+
+  def votes_for_poll(poll_id)
+    UserOption.joins(:option).where(option: { poll_id: poll_id })
   end
 end
