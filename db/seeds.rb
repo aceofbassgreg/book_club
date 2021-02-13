42cacefe1d423d235982d2ceb3e6bcb1dafe3c72
gkenenitz <gkenenitz@apple.com>
2021-02-13 13:39:46 +0900

    Updates seeds to be more realistic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,70 @@
 ### Create Polls
-poll = Poll.find_or_create_by(name: "January Poll", active_starting_at: Date.yesterday - 50000, active_ending_at: Date.yesterday)
-(1..3).to_a.each do |i|
-  poll.books.create(title: "Book #{i}", author: "Dr. Seuss")
-end
+nov_poll = Poll.find_or_create_by(
+  name: 'November 2020 Poll',
+  active_starting_at: Time.new(2020, 10, 1),
+  active_ending_at: Time.new(2020, 10, 16)
+)
+nov_poll.books.create(
+  title: 'The City We Became',
+  author: 'N.K. Jemison',
+  link: 'https://openlibrary.org/works/OL20656799W/The_City_We_Became'
+)
+nov_poll.books.create(
+  title: 'Blood Meridian',
+  author: 'Cormac McCarthy',
+  link: 'https://openlibrary.org/works/OL40879W/Blood_Meridian_or_the_Evening_Redness_in_the_West')
+nov_poll.books.create(
+  title: 'Pride and Prejudice',
+  author: 'Jane Austen',
+  link: 'https://openlibrary.org/works/OL9039929W/Pride_and_Prejudice'
+)
 
-poll = Poll.find_or_create_by(name: "February Poll", active_starting_at: Date.yesterday - 5000, active_ending_at: Date.yesterday)
+dec_poll = Poll.find_or_create_by(
+  name: 'December 2020 Poll',
+  active_starting_at: Time.new(2020, 12, 1),
+  active_ending_at: Time.new(2020, 12, 14)
+)
 
-(4..6).to_a.each do |i|
-  poll.books.create(title: "Book #{i}", author: "Dr. Seuss")
-end
+dec_poll.books.create(
+  title: 'Braiding Sweetgrass',
+  author: 'Robin Wall Kimmerer',
+  link: 'https://openlibrary.org/works/OL17848523W/Braiding_sweetgrass'
+)
+dec_poll.books.create(
+  title: 'Infinite Jest',
+  author: 'David Foster Wallace',
+  link: 'https://openlibrary.org/works/OL2943602W/Infinite_jest'
+)
 
-poll = Poll.find_or_create_by(name: "March Poll", active_starting_at: Date.yesterday, active_ending_at: Date.tomorrow + 50000)
+current_poll = Poll.find_or_create_by(
+  name: 'Current Active Poll',
+  active_starting_at: Time.now,
+  active_ending_at: Time.now + 1_000_000
+)
 
-(7..9).to_a.each do |i|
-  poll.books.create(title: "Book #{i}", author: "Dr. Seuss")
-end
+current_poll.books.create(
+  title: 'Florida',
+  author: 'Lauren Groff',
+  link: 'https://openlibrary.org/works/OL19748408W/Florida'
+)
 
-### Create User
-User.find_or_create_by(email: "gk@gk.com", first_name: "Greg", last_name: "Kenenitz").update(password: "password99")
+current_poll.books.create(
+  title: "Gravity's Rainbow",
+  author: 'Thomas Pynchon',
+  link: 'https://openlibrary.org/works/OL2636675W/Gravity%27s_Rainbow'
+)
+
+current_poll.books.create(
+  title: 'Frankenstein',
+  author: 'Mary Shelley',
+  link: 'https://openlibrary.org/works/OL450125W/Frankenstein'
+)
+
+current_poll.books.create(
+  title: 'The Sound and The Fury',
+  author: 'William Faulkner',
+  link: 'https://openlibrary.org/works/OL82870W/The_Sound_and_the_Fury'
+)
+
+### Create Admin User
+User.find_or_create_by(email: 'gk@gk.com', first_name: 'Greg', last_name: 'Kenenitz', admin: true).update(password: 'password99')
