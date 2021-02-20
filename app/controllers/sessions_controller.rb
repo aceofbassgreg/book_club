8398c98a686da2c8825f9f414049aaafbb9812a7
gkenenitz <gkenenitz@apple.com>
2021-02-20 14:52:03 +0900

    Display flash alerts and notices with styling, add flash to SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,17 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user&.authenticate(params[:password])
       session[:current_user_id] = @user.id
+      flash[:notice] = 'Login successful.'
       redirect_to events_url
     else
-      redirect_to '/sessions/new'
+      flash.alert = 'Login failed.'
+      render :new
     end
   end
 
   def destroy
     session[:current_user_id] = nil
-    redirect_to '/polls'
+    flash[:notice] = 'Signed out.'
+    redirect_to events_url
   end
 end
