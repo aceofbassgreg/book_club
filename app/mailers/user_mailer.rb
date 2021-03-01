ab36fe98b482947a69db447fd81f8c155a16f666
gkenenitz <gkenenitz@apple.com>
2021-03-01 14:14:25 +0900

    Update header & mailer to reflect inviting users to app, rather than welcoming or creating users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,13 @@
 class UserMailer < ApplicationMailer
-
-  def welcome_email
+  def invite_email
     @user = params[:user]
-    @url  = ENV['REGISTRATION_URL']
+    @url  = ENV['REGISTRATION_URL'] + user_hash
     mail(to: @user.email, subject: 'Welcome to the Eclectic Book Club App')
+  end
+
+  private
+
+  def user_hash
+    Digest::SHA256.hexdigest("#{@user.first_name}+#{@user.email}")
   end
 end
