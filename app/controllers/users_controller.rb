d0c5582615c58cd046b0ac86d835b5871be42a9c
gkenenitz <gkenenitz@apple.com>
2021-03-06 21:14:05 +0900

    Create new invitation form, change user view to refer to registration instead of create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      flash.notice = 'Thanks for joining!'
+      redirect_to events_url
+    else
+      flash.alert = "User did not create successfully: #{@user.errors.full_messages.to_sentence}."
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :admin)
   end
 end
