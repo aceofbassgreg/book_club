58b41139d6c8feec232b03a344820ce0acbf7261
gkenenitz <gkenenitz@apple.com>
2021-03-07 14:10:22 +0900

    Allow multiple invites to be created

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,18 +1,21 @@
 class InvitesController < ApplicationController
   def new
     @invite = Invite.new
+    @invites = [@invite]
   end
 
   def create
-    @invite = Invite.new(invite_params)
-    if @invite.save
-      UserMailer.with(user: @invite).invite_email.deliver_later
+    invite_params.each do |param|
+      @invite = Invite.new(param)
+      if @invite.save
+        UserMailer.with(user: @invite).invite_email.deliver_later
+      end
     end
   end
 
   private
 
   def invite_params
-    params.require(:invite).permit(:email, :first_name)
+    params.require(:invites).map { |invite| invite.permit(:email, :first_name) }
   end
 end
