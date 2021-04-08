25d03a6d0d5990c744b330e8ad1dd156cf557709
gkenenitz <gkenenitz@apple.com>
2021-04-08 16:42:49 +0900

    Makes removeBooks functionality compatible with pre-loaded books

--- app/javascript/packs/removeFieldset.js
+++ app/javascript/packs/removeFieldset.js
@@ -1,35 +1,40 @@
 const removeFieldset = ()=> {
-  // Case #1: Initial page load, only one Remove Book button
-  const removeButton = document.getElementsByClassName("removeFieldset")[0];
-  removeButton.addEventListener("click", () => {
-    if ( document.querySelector("#fieldsetContainer").childElementCount === 1 ) {
-      const descendents = removeButton.parentElement.getElementsByTagName("*");
-      for (let descendent of descendents) {
-        if (descendent.tagName === "INPUT") {
-          descendent.value = ""
+  // Case #1: Initial page load, or books are pre-loaded
+  const removeButtons = document.getElementsByClassName("removeFieldset");
+  for (let element of removeButtons) {
+    element.addEventListener("click", () => {
+      if (removeButtons.length == 1) {
+        const descendents = element.parentElement.getElementsByTagName("*");
+        for (let descendent of descendents) {
+          if (descendent.tagName === "INPUT") {
+            descendent.value = ""
+          }
         }
+      } else {
+        const fieldset = element.parentElement;
+        fieldset.remove()
       }
-    }
-  })
+    })
+  };
 
-  // Case #2: User has added more books by clicking "Add more books"
+  // Case #2: User has added more bookks (and more removeButtons) by clicking "Add more books"
   const createButton = document.getElementsByClassName("addFieldset")[0];
   createButton.addEventListener("click", () => {
-    const removeButtons = document.getElementsByClassName("removeFieldset");
-    for (let element of removeButtons) {
-      element.addEventListener("click", () => {
-        const fieldset = element.parentElement;
-        if ( document.querySelector("#fieldsetContainer").childElementCount === 1 ) {
-          const descendents = fieldset.getElementsByTagName("*");
-          for (let descendent of descendents) {
-            if (descendent.tagName === "INPUT") {
-              descendent.value = ""
-            }
+  const removeButtons = document.getElementsByClassName("removeFieldset");
+  for (let element of removeButtons) {
+    element.addEventListener("click", () => {
+      if (removeButtons.length == 1) {
+        const descendents = element.parentElement.getElementsByTagName("*");
+        for (let descendent of descendents) {
+          if (descendent.tagName === "INPUT") {
+            descendent.value = ""
           }
-        } else {
-          fieldset.remove();
         }
-      })
- }})
-};
+      } else {
+        const fieldset = element.parentElement;
+        fieldset.remove()
+      }
+    })
+  };
+})};
 export { removeFieldset };
