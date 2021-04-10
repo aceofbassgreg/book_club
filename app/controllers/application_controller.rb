55662d18e4cca7211cf187e487fc08f382fa55a9
gkenenitz <gkenenitz@apple.com>
2021-04-11 01:33:17 +0900

    Require users to be logged in (oops)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
+  before_action :require_login
+
+  def require_login
+    return if logged_in?
+
+    flash[:error] = "You must be logged in to access this section"
+    redirect_to login_url # halts request cycle
+  end
 
   def current_user
     @current_user ||= User.find_by(id: session[:current_user_id])
